c493b2ad0dc92f4638b5153def632e7144ce214b
saracollu <saracollu.ws@gmail.com>
2026-03-08 21:42:58 +0100

    exercise 8 arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -46,4 +46,5 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty);
