739b36ad5b713e06efda3eb3947a7f0abe95d74e
saracollu <saracollu.ws@gmail.com>
2026-03-08 21:39:01 +0100

    5 exercise arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -25,9 +25,13 @@ console.log(lastTeacher);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa')
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
