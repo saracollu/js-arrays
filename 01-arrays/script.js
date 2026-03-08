7cb53013fe75f0839c076c80d5989538b3b10f91
saracollu <saracollu.ws@gmail.com>
2026-03-08 21:40:01 +0100

    6 exercise arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -30,10 +30,13 @@ console.log(firstTeacher);
 
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push('Vanessa')
+teachers.push('Vanessa');
 console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift('Sarah');
+console.log(teachers);
+
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
